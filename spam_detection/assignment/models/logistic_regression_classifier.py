1b6ed8272b63d506d4a98f0bfabad2cd2435e3cb
giorgio <giorgiosaldana99@gmail.com>
2024-08-30 10:34:48 +0000

    [add] Added the logistic regression model to classify spam or not spam mails

---
+++ spam_detection/assignment/models/logistic_regression_classifier.py
@@ -0,0 +1,50 @@
+"""
+This script implements the Logistic Regression classifier for spam detection.
+It uses the `LogisticRegression` class from `sklearn` to train the model and predict the labels for the test set.
+It also calculates the accuracy, precision, recall, and F1 score for each C value and plots the ROC curve for each C value.
+"""
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import roc_curve, auc, classification_report
+
+from utils.vectorizator import vectorization
+from utils.data_analysis import prepare_data, calculate_performance_metrics, save_plot, compute_plot_roc_curve, \
+    aggregate_classification_reports, display_classification_reports
+
+performance_metrics = []
+c = [0.01, 0.1, 1, 10, 100, 1000, 1e4, 1e5]
+reports = {}
+
+for c_val in c:
+    # Vectorize the training and testing datasets
+    df_train = vectorization('../../dataset/train-mails', 1000)
+    df_test = vectorization('../../dataset/test-mails', 1000)
+
+    # Separate features and labels for training and test data
+    X_train, y_train, X_test, y_test = prepare_data(df_train, df_test)
+
+    # Initialize and train the Logistic Regression model
+    clf = LogisticRegression(C=c_val)
+    clf.fit(X_train, y_train)
+
+    # Predict the labels for the test set
+    y_test_pred = clf.predict(X_test)
+
+    # Calculate and store the performance metrics
+    calculate_performance_metrics(clf, X_test, y_test, y_test_pred, performance_metrics, 1000)
+
+    # Generate and plot the ROC curve among the different dictionary dimensions
+    fpr, tpr, _ = roc_curve(y_test, y_test_pred)
+    roc_auc = auc(fpr, tpr)
+
+    compute_plot_roc_curve(fpr, tpr, f'ROC Curve (AUC = {roc_auc:.2f})')
+
+    # Store the classification report for later aggregation
+    reports[c_val] = classification_report(y_test, y_test_pred, output_dict=True)
+
+# save the plot and show it
+save_path = 'resources/lr_roc_curve.png'
+save_plot(save_path)
+
+# Print the performance metrics
+result_df = aggregate_classification_reports(reports)
+display_classification_reports(result_df)
