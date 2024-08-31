1f4e57009b3907513c8f2b555ab2572dd6f011b0
giorgio <giorgiosaldana99@gmail.com>
2024-08-31 16:11:29 +0000

    [edit] Edited the models to divide the logic and create functions that allow to accomplish the assignment

--- spam_detection/assignment/models/logistic_regression_classifier.py
+++ spam_detection/assignment/models/logistic_regression_classifier.py
@@ -4,47 +4,95 @@ It uses the `LogisticRegression` class from `sklearn` to train the model and pre
 It also calculates the accuracy, precision, recall, and F1 score for each C value and plots the ROC curve for each C value.
 """
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import roc_curve, auc, classification_report
+from sklearn.metrics import roc_curve, auc, classification_report, roc_auc_score
 
 from utils.vectorizator import vectorization
 from utils.data_analysis import prepare_data, calculate_performance_metrics, save_plot, compute_plot_roc_curve, \
     aggregate_classification_reports, display_classification_reports
 
 performance_metrics = []
-c = [0.01, 0.1, 1, 10, 100, 1000, 1e4, 1e5]
+c = [0.0001, 0.1, 1, 10, 1e4]
 reports = {}
 
-for c_val in c:
-    # Vectorize the training and testing datasets
-    df_train = vectorization('../../dataset/train-mails', 1000)
-    df_test = vectorization('../../dataset/test-mails', 1000)
+def train_best_model(X_train, y_train, X_test, y_test, c_values):
 
-    # Separate features and labels for training and test data
-    X_train, y_train, X_test, y_test = prepare_data(df_train, df_test)
+    best_recall = 0
+    best_c = None
+    best_fpr, best_tpr = None, None
+    best_roc_auc = None
 
-    # Initialize and train the Logistic Regression model
-    clf = LogisticRegression(C=c_val)
-    clf.fit(X_train, y_train)
+    for c_val in c_values:
+        # Initialize and train the Logistic Regression model
+        clf = LogisticRegression(C=c_val)
+        clf.fit(X_train, y_train)
 
-    # Predict the labels for the test set
-    y_test_pred = clf.predict(X_test)
+        # Prediction for training set
+        y_train_pred = clf.predict(X_train)
+        y_train_prob = clf.predict_proba(X_train)[:, 1]
 
-    # Calculate and store the performance metrics
-    calculate_performance_metrics(clf, X_test, y_test, y_test_pred, performance_metrics, 1000)
+        # Calculate performance metrics
+        report = classification_report(y_train, y_train_pred, output_dict=True)
+        recall = report['1']['recall']
 
-    # Generate and plot the ROC curve among the different dictionary dimensions
-    fpr, tpr, _ = roc_curve(y_test, y_test_pred)
-    roc_auc = auc(fpr, tpr)
+        # Calculate the ROC curve and AUC
+        fpr, tpr, _ = roc_curve(y_train, y_train_prob)
+        roc_auc = roc_auc_score(y_train, y_train_prob)
 
-    compute_plot_roc_curve(fpr, tpr, f'ROC Curve (AUC = {roc_auc:.2f})')
+        if recall > best_recall:
+            best_recall = recall
+            best_c = c_val
+            best_fpr = fpr
+            best_tpr = tpr
+            best_roc_auc = roc_auc
 
-    # Store the classification report for later aggregation
-    reports[c_val] = classification_report(y_test, y_test_pred, output_dict=True)
+    return best_recall, best_fpr, best_tpr, best_roc_auc, best_c
 
-# save the plot and show it
-save_path = 'resources/lr_roc_curve.png'
-save_plot(save_path)
+def run_logistic_regression():
 
-# Print the performance metrics
-result_df = aggregate_classification_reports(reports)
-display_classification_reports(result_df)
+    for c_val in c:
+
+        # Vectorize the training and testing datasets
+        df_train = vectorization('../../dataset/train-mails', 1000)
+        df_test = vectorization('../../dataset/test-mails', 1000)
+
+        # Separate features and labels for training and test data
+        X_train, y_train, X_test, y_test = prepare_data(df_train, df_test)
+
+        # Initialize and train the Logistic Regression model
+        clf = LogisticRegression(C=c_val)
+        clf.fit(X_train, y_train)
+
+        # Prediction for training set
+        y_train_pred = clf.predict(X_train)
+        y_train_prob = clf.predict_proba(X_train)[:, 1]  # Probabilities for the positive class
+
+        # Predict the labels for the test set
+        y_test_pred = clf.predict(X_test)
+        y_test_prob = clf.predict_proba(X_test)[:, 1]  # Probabilities for the positive class
+
+        # Calculate and store the performance metrics
+        calculate_performance_metrics(clf, X_test, y_test, y_test_pred, performance_metrics, 1000)
+
+        # Generate and plot the ROC curve using probabilities
+        fpr, tpr, _ = roc_curve(y_test, y_test_prob)
+        roc_auc = roc_auc_score(y_test, y_test_prob)
+
+        compute_plot_roc_curve(fpr, tpr, f'Logistic Regression (C={c_val}, AUC = {roc_auc:.3f})')
+
+        # Store the classification report for later aggregation (for both training and test)
+        reports[f'Training_C_{c_val}'] = classification_report(y_train, y_train_pred, output_dict=True)
+        reports[f'Test_C_{c_val}'] = classification_report(y_test, y_test_pred, output_dict=True)
+
+        # Store the classification report for later aggregation
+        # reports[c_val] = classification_report(y_test, y_test_pred, output_dict=True)
+
+    # save the plot and show it
+    save_path = 'resources/lr_roc_curve.png'
+    save_plot(save_path)
+
+    # Print the performance metrics
+    result_df = aggregate_classification_reports(reports)
+    display_classification_reports(result_df)
+
+if __name__ == '__main__':
+    run_logistic_regression()
--- spam_detection/assignment/models/naive_bayes_classifier.py
+++ spam_detection/assignment/models/naive_bayes_classifier.py
@@ -5,7 +5,7 @@ calculates the accuracy, precision, recall, and F1 score for each dictionary dim
 dimension.
 """
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import roc_curve, auc, classification_report
+from sklearn.metrics import roc_curve, auc, classification_report, roc_auc_score
 
 from utils.vectorizator import vectorization
 from utils.data_analysis import prepare_data, calculate_performance_metrics, save_plot, compute_plot_roc_curve, \
@@ -16,38 +16,64 @@ performance_metrics = []
 dict_dim = [100, 500, 1000, 2000, 3000]
 reports = {}
 
-for dim in dict_dim:
-
-    # Vectorize the training and testing datasets
-    df_train = vectorization('../../dataset/train-mails', dim)
-    df_test = vectorization('../../dataset/test-mails', dim)
-
-    # Separate features and labels for training and test data
-    X_train, y_train, X_test, y_test = prepare_data(df_train, df_test)
-
-    # Initialize and train the Multinomial Naive Bayes model
+# The dictionary dimension is not passed as a parameter because it is a global variable used in evaluator.py
+def train_best_model(X_train, y_train, X_test, y_test):
     clf = MultinomialNB()
     clf.fit(X_train, y_train)
 
-    # Predict the labels for the test set
     y_test_pred = clf.predict(X_test)
+    y_test_prob = clf.predict_proba(X_test)[:, 1]
 
-    # Calculate and store the performance metrics
-    calculate_performance_metrics(clf, X_test, y_test, y_test_pred, performance_metrics, dim)
+    # Calculate performance metrics
+    report = classification_report(y_test, y_test_pred, output_dict=True)
+    recall = report['1']['recall']
 
-    # Generate and plot the ROC curve among the different dictionary dimensions
-    fpr, tpr, _ = roc_curve(y_test, y_test_pred)
-    roc_auc = auc(fpr, tpr)
+    # Calculate the ROC curve and AUC
+    fpr, tpr, _ = roc_curve(y_test, y_test_prob)
+    roc_auc = roc_auc_score(y_test, y_test_prob)
 
-    compute_plot_roc_curve(fpr, tpr, f'Dictionary Dimension = {dim} (AUC = {roc_auc:.2f})')
+    return recall, fpr, tpr, roc_auc
 
-    # Store the classification report for later aggregation
-    reports[dim] = classification_report(y_test, y_test_pred, output_dict=True)
+def run_naive_bayes():
 
-# save the plot and show it
-save_path = 'resources/naive_bayes_roc_curve.png'
-save_plot(save_path)
+    for dim in dict_dim:
 
-# Print the performance metrics
-result_df = aggregate_classification_reports(reports)
-display_classification_reports(result_df)
+        # Vectorize the training and testing datasets
+        df_train = vectorization('../../dataset/train-mails', dim)
+        df_test = vectorization('../../dataset/test-mails', dim)
+
+        # Separate features and labels for training and test data
+        X_train, y_train, X_test, y_test = prepare_data(df_train, df_test)
+
+        # Initialize and train the Multinomial Naive Bayes model
+        clf = MultinomialNB()
+        clf.fit(X_train, y_train)
+
+        # Predict the labels for the test set
+        y_test_pred = clf.predict(X_test)
+
+        # **Predict probabilities for ROC AUC**
+        y_test_prob = clf.predict_proba(X_test)[:, 1]
+
+        # Calculate and store the performance metrics
+        calculate_performance_metrics(clf, X_test, y_test, y_test_pred, performance_metrics, dim)
+
+        # Generate and plot the ROC curve using probabilities
+        fpr, tpr, _ = roc_curve(y_test, y_test_prob)
+        roc_auc = roc_auc_score(y_test, y_test_prob)
+
+        compute_plot_roc_curve(fpr, tpr, f'Dictionary Dimension = {dim} (AUC = {roc_auc:.3f})')
+
+        # Store the classification report for later aggregation
+        reports[dim] = classification_report(y_test, y_test_pred, output_dict=True)
+
+    # save the plot and show it
+    save_path = 'resources/naive_bayes_roc_curve.png'
+    save_plot(save_path)
+
+    # Print the performance metrics
+    result_df = aggregate_classification_reports(reports)
+    display_classification_reports(result_df)
+
+if __name__ == '__main__':
+    run_naive_bayes()
