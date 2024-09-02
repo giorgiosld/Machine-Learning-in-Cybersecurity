47db381fcbc6c0a4bb412dbb2db0bd2d067a02bd
giorgio <giorgiosaldana99@gmail.com>
2024-09-02 10:44:48 +0000

    [edit] Added the possibility to train the svm using different kernel to find the best suitable solution for spam classification

--- spam_detection/assignment/models/svm_classifier.py
+++ spam_detection/assignment/models/svm_classifier.py
@@ -12,32 +12,47 @@ from utils.data_analysis import prepare_data, save_plot, compute_plot_roc_curve,
 
 # Initialize parameters used for the experiment
 performance_metrics = []
+kernels = ['linear', 'poly', 'rbf', 'sigmoid']
 reports = {}
 
-def run_svm(X_train, y_train, X_test, y_test):
 
-    print("Running Support Vector Machine with linear kernel")
+def run_svm(X_train, y_train, X_test, y_test, kernels):
+    best_recall = 0
+    best_k = None
+    best_fpr, best_tpr = None, None
+    best_roc_auc = None
 
-    # Initialize and train the SVM model
-    svm_clf = SVC(kernel='linear', probability=True)
-    svm_clf.fit(X_train, y_train)
+    print(f"Running Support Vector Machine with different kernels: {kernels}")
 
-    # Predict probabilities for ROC AUC
-    y_test_prob = svm_clf.predict_proba(X_test)[:, 1]
+    for kernel in kernels:
 
-    # Predict the labels for the test set
-    y_test_pred = svm_clf.predict(X_test)
+        # Initialize and train the SVM model
+        svm_clf = SVC(kernel=kernel, probability=True)
+        svm_clf.fit(X_train, y_train)
 
-    # Calculate metrics
-    report = classification_report(y_test, y_test_pred, output_dict=True)
+        # Predict probabilities for ROC AUC
+        y_test_prob = svm_clf.predict_proba(X_test)[:, 1]
 
-    # Generate and plot the ROC curve using probabilities
-    fpr, tpr, _ = roc_curve(y_test, y_test_prob)
-    roc_auc = roc_auc_score(y_test, y_test_prob)
+        # Predict the labels for the test set
+        y_test_pred = svm_clf.predict(X_test)
 
-    compute_plot_roc_curve(fpr, tpr, f'SVM (AUC = {roc_auc:.3f})')
+        # Generate and plot the ROC curve using probabilities
+        fpr, tpr, _ = roc_curve(y_test, y_test_prob)
+        roc_auc = roc_auc_score(y_test, y_test_prob)
 
-    reports['Linear'] = report
+        compute_plot_roc_curve(fpr, tpr, f'SVM (kernel={kernel}, AUC = {roc_auc:.3f})')
+
+        # Calculate metrics
+        report = classification_report(y_test, y_test_pred, output_dict=True)
+        recall = report['1']['recall']
+        if recall > best_recall:
+            best_recall = recall
+            best_kernel = kernel
+            best_fpr = fpr
+            best_tpr = tpr
+            best_roc_auc = roc_auc
+
+        reports[kernel] = report
 
     save_path = 'resources/svm_roc_curve.png'
     save_plot(save_path)
@@ -46,13 +61,16 @@ def run_svm(X_train, y_train, X_test, y_test):
     result_df = aggregate_classification_reports(reports)
     display_classification_reports(result_df)
 
-    return report['1']['recall'], fpr, tpr, roc_auc
+    return best_recall, best_fpr, best_tpr, best_roc_auc, best_kernel
+
 
 if __name__ == '__main__':
-    # Load the training and testing datasets
-    df_train = vectorization('../../dataset/train-mails', 2000)
-    df_test = vectorization('../../dataset/test-mails', 2000)
+    PATH = '../../dataset/'
+
+    # Vectorize the training and testing the datasets with different models
+    df_train = vectorization(PATH + 'train-mails', 2000)
+    df_test = vectorization(PATH + 'test-mails', 2000)
 
     # Prepare the data for training and testing
     X_train, y_train, X_test, y_test = prepare_data(df_train, df_test)
-    run_svm(X_train, y_train, X_test, y_test)
+    run_svm(X_train, y_train, X_test, y_test, kernels)
