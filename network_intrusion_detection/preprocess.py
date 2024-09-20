e589c95d30b6a6c512c587d2ed993481edb3541f
giorgio <giorgiosaldana99@gmail.com>
2024-09-20 23:11:07 +0000

    [edit] Added the aggregation of different vaues in 'Label' feature and plotted the distribution to have a better understanding of dataset

--- network_intrusion_detection/preprocess.py
+++ network_intrusion_detection/preprocess.py
@@ -3,21 +3,49 @@ import matplotlib.pyplot as plt
 
 import os
 
-# TODO: fix the umbalnced data because the Label is classified only by BENIGN and the rest are classified as OTHERS
-def visualize_dataframe(df):
-    # Convert 'Label' to numerical values (BENIGN -> 0, others -> 1)
-    df['Label_numeric'] = df['Label'].apply(lambda x: 0 if x == 'BENIGN' else 1)
+# Define a dictionary for the label mapping
+label_mapping = {
+    'benign': 'Benign',
+    'dos': 'DoS',
+    'ddos': 'DoS',
+    'scan': 'PortScan'
+}
 
-    # Extract feature values  and labels
-    xv = df[['Flow_Duration', 'Total_Fwd_Packets']].values
-    yv = df['Label_numeric'].values
+"""
+Aggregate the labels in the dataset. This function converts the labels to lowercase and maps them to the corresponding
+"""
+def aggregate_labels(df):
+    df.columns = df.columns.str.strip()
+    print(df['Label'].unique())
+    df['Label'] = df['Label'].str.lower().apply(
+        lambda x: next((value for key, value in label_mapping.items() if key in x), 'Exploit')
+    )
+    print(df.head())
+    print(df['Label'].unique())
+
+
+"""
+One hot encode the Label column getting a new column for each Label value
+:param df: The dataframe where analyze the code
+"""
+def one_hot_encoding(df):
+    print(df['Label'].unique())
+    df = pd.get_dummies(df, columns=['Label'])
+    print(f"Columns after one-hot encoding: \n {df.head()}")
+
+"""
+Plot the distribution of different values in Label feature
+:param df: The dataframe to be analyzed
+"""
+def plot_distribution(df):
+    label_counts = df['Label'].value_counts()
+    # Plot the distribution of labels
     plt.figure(figsize=(10, 6))
-    plt.scatter(xv[yv == 0, 0], xv[yv == 0, 1], label="BENIGN", alpha=0.5, linewidth=0.2)
-    plt.scatter(xv[yv == 1, 0], xv[yv == 1, 1], label="Others", alpha=0.5, linewidth=0.2, color='r')
-    plt.xlabel('Flow Duration')
-    plt.ylabel('Total Fwd Packets')
-    plt.title('Flow Duration vs Total Fwd Packets')
-    plt.legend()
+    label_counts.plot(kind='bar')
+    plt.title('Distribution of Labels in the Dataset')
+    plt.xlabel('Label')
+    plt.ylabel('Count')
+    plt.xticks(rotation=45)
     plt.show()
 
 """
@@ -32,9 +60,6 @@ def see_insight(df):
     df.columns = df.columns.str.replace(' ', '_')
     print(f"Features column in the dataset: {df.columns}")
     print(f"Null entries in the dataset: {df.isnull().sum().max()} ")
-    print(f"Query the dataset: {df.info()} ")
-    # Filter data by each label
-    print(f"Label distribution: {df[df.Label == 'BENIGN'].Destination_Port.describe()}")
 
 """
 This function read all CSV files, apply the preprocessing steps, split processed data in two Pandas DataFrames, and
@@ -47,8 +72,11 @@ def get_dataset(path_to_files, splitmode):
     list_csv = os.listdir(path_to_files)
     # Create a single DataFrame with all the data
     df = pd.concat([pd.read_csv(path_to_files + f) for f in list_csv if f.endswith('.csv')])
-    see_insight(df)
-    visualize_dataframe(df)
+
+    aggregate_labels(df)
+    # see_insight(df)
+    plot_distribution(df)
+    # one_hot_encoding(df)
     d_training, d_testing = None, None
     return d_training, d_testing
 
