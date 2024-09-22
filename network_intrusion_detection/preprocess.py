08f4b3bacfcb51eb512ea5de4bc563f323de7553
giorgio <giorgiosaldana99@gmail.com>
2024-09-22 18:33:39 +0000

    [edit] Added the features to handle missing data and encode 'Label' column from dataset

--- network_intrusion_detection/preprocess.py
+++ network_intrusion_detection/preprocess.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 
 import os
 
@@ -13,6 +14,7 @@ class Preprocess:
         path_to_files: The path to the directory containing the CSV files
         splitmode: The mode to split the data. If "floating", the data is split into randomize subset, otherwise use day to split
         label_mapping: A dictionary to map the labels to the corresponding values
+        label_encoder: The label encoder to encode the labels
     """
 
     def __init__(self, path_to_files, splitmode="none"):
@@ -29,6 +31,7 @@ class Preprocess:
             'ddos': 'DoS',
             'scan': 'PortScan'
         }
+        self.label_encoder = LabelEncoder()
 
     def aggregate_labels(self, df):
         """
@@ -40,6 +43,38 @@ class Preprocess:
         df['Label'] = df['Label'].str.lower().apply(
             lambda x: next((value for key, value in self.label_mapping.items() if key in x), 'Exploit')
         )
+
+    def encode_labels(self, df):
+        """
+        Encode the labels in the dataset using LabelEncoder
+        """
+        df['Encoded_Label'] = self.label_encoder.fit_transform(df['Label'])
+
+    def ensure_numeric(self, df):
+        """
+        Ensures all columns in the dataframe are numeric. If a column contains non-numeric data, it will be converted or dropped.
+        """
+        # Select columns that are not numeric
+        non_numeric_columns = df.select_dtypes(exclude=['number']).columns
+        for col in non_numeric_columns:
+            try:
+                df[col] = pd.to_numeric(df[col], errors='coerce')
+            except Exception as e:
+                print(f"Could not convert {col}: {e}")
+
+        # Replace Inf/-Inf with NaN
+        df.replace([float('inf'), float('-inf')], float('nan'), inplace=True)
+
+        df.fillna(df.mean(), inplace=True)
+
+        return df
+
+    def get_label_mapping(self):
+        """
+        Get the mapping of encoded labels to their original string values
+        """
+        return dict(zip(self.label_encoder.classes_, self.label_encoder.transform(self.label_encoder.classes_)))
+
 
     @staticmethod
     def plot_distribution(df):
@@ -65,6 +100,7 @@ class Preprocess:
         :param df: The DataFrame to be analyzed
         :return: The function prints the insight of the dataset in place
         """
+        print(f"First five rows of the dataset: \n{df.head()}")
         print(f"Null entries in the dataset: \n{df.isnull().sum().max()} ")
         print(f"Label column: \n {df[df.Label == 'Benign'].Label.describe()}")
         print(f"Values in column Label: \n {df['Label'].value_counts()}")
@@ -94,16 +130,19 @@ class Preprocess:
         self.aggregate_labels(d_train)
         self.aggregate_labels(d_test)
 
+        self.encode_labels(d_train)
+        self.encode_labels(d_test)
+
+        d_train.drop(columns=['Label'], inplace=True)
+        d_test.drop(columns=['Label'], inplace=True)
+
+        self.ensure_numeric(d_train)
+        self.ensure_numeric(d_test)
+
         if isinstance(self.splitmode, float) and 0 < self.splitmode < 1:
             combined_df = pd.concat([d_train, d_test], ignore_index=True)
             d_train, d_test = train_test_split(combined_df, test_size=(1 - self.splitmode), random_state=42,
-                                               stratify=combined_df['Label'])
-
-        self.see_insight(d_train)
-        self.see_insight(d_test)
-
-        self.plot_distribution(d_train)
-        self.plot_distribution(d_test)
+                                               stratify=combined_df['Encoded_Label'])
 
         return d_train, d_test
 
@@ -112,4 +151,5 @@ if __name__ == "__main__":
     path_to_files = "MachineLearningCVE/"
     preprocessor = Preprocess(path_to_files, 0.8)
     d_train, d_test = preprocessor.get_dataset()
+    print(preprocessor.see_insight(d_train))
 
