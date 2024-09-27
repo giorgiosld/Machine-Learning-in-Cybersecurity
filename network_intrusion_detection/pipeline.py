11ab942e1c4315ce0901947cc354161f61ebb20e
giorgio <giorgiosaldana99@gmail.com>
2024-09-27 16:00:02 +0000

    [edit] Finished the report that describe each tasks about NIDS experiments

--- network_intrusion_detection/pipeline.py
+++ network_intrusion_detection/pipeline.py
@@ -2,9 +2,11 @@ from model_trainer import ModelTrainer
 from preprocess import Preprocess
 from resampler import Resampler
 
-
+# Path to the directory containing the CSV files
 path = 'MachineLearningCVE'
 
+# To use different split mode define for floating a value between 0 and 1 as parameter in Preprocess (e.g. 0.6)
+# To use day as split mode define 'none' as parameter in Preprocess
 preprocessor = Preprocess(path, 0.6)
 d_train, d_test = preprocessor.get_dataset()
 
@@ -16,6 +18,7 @@ resampler = Resampler()
 X_train_sampled, y_train_sampled = resampler.balanced_undersample(X_train, y_train)
 X_test_sampled, y_test_sampled = resampler.balanced_undersample(X_test, y_test)
 
+# To use DT or RF define for DT 'decision_tree' and for RF 'random_forest' as parameter in ModelTrainer
 clf = ModelTrainer('random_forest')
 
 clf.fit(X_train, y_train)
